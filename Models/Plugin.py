97792b8a82806c4f61b4687354d2a94e0d831e2b
twof <fabiobean2@gmail.com>
2016-12-20 13:33:48 -0800

    Plugin is no longer a singleton

--- Models/Plugin.py
+++ Models/Plugin.py
@@ -8,26 +8,10 @@
 from Utils.constants import Plugin_Type
 
 
-# Plugin singleton
 class Plugin(object):
-    class _Plugin:
-        def __init__(self, match_type=Plugin_Type.everything, query=None):
-            self.match_type = match_type
-            self.query = query
-
-    instance = None
-
     def __init__(self, match_type=Plugin_Type.everything, query=None):
-        if not Plugin.instance:
-            Plugin.instance = Plugin._Plugin(match_type, query)
-        else:
-            Plugin.instance.match_type = match_type
-            Plugin.instance.query = query
         self.match_type = match_type
         self.query = query
-
-    def __getattr__(self, name):
-        return getattr(self.instance, name)
 
     def callback(reply, message):
         raise NotImplementedError("Subclass must implement abstract method")
