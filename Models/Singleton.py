27b0d02d38ccd211814ba2f6c1b9c202ad791548
twof <fabiobean2@gmail.com>
2016-12-29 20:11:05 -0800

    Simplified Singleton class

--- Models/Singleton.py
+++ Models/Singleton.py
@@ -1,12 +1,21 @@
+"""
+Implemented by plugins. Ensures that plugins are singletons.
+"""
+
+
 class Singleton(object):
+    """
+    Implemented by plugins. Ensures that plugins are singletons.
+    """
     _instance = None
 
     @staticmethod
     def __new__(class_, *args, **kwargs):
-        print(class_._instance)
-        print(class_)
+        """
+        Spits out the instance if it exsits and creates a new one if it
+        does not.
+        Standard Singleton pattern
+        """
         if not isinstance(class_._instance, class_):
             class_._instance = object.__new__(class_, *args, **kwargs)
-            return class_._instance
-        else:
-            return class_._instance
+        return class_._instance
